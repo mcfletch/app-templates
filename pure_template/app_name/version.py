4f11d17b1391e4a17c393c9ac4c40e67e29ee5a8
Mike C. Fletcher <mcfletch@vrplumber.com>
2013-04-11 08:53:46 -0400

    Template for simple Python packages

---
+++ pure_template/app_name/version.py
@@ -0,0 +1,2 @@
+"""Version counter for {{app_name}}"""
+__version__ = '1.0.0'
---
+++ pure_template/setup.py
@@ -0,0 +1,47 @@
+import os
+
+from setuptools import setup, find_packages
+version = [
+    (line.split('=')[1]).strip().strip('"').strip("'")
+    for line in open(os.path.join('{{app_name}}','version.py'))
+    if line.startswith( '__version__' )
+][0]
+
+if __name__ == "__main__":
+    setup(
+        name='{{app_name}}',
+        version='1.0.0',
+        description='{{app_name}}',
+        long_description='{{app_name}}',
+        classifiers=[
+            "Programming Language :: Python",
+            "Framework :: Django",
+            "Topic :: Internet :: WWW/HTTP",
+            "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+        author='VRPlumber Consulting Inc.',
+        author_email='mcfletch@vrplumber.com',
+        url='http://www.vrplumber.com/programming/project/{{app_name}}',
+        keywords='',
+        packages=find_packages(),
+        include_package_data=True,
+        license='Commercial',
+        # Dev-only requirements:
+        # nose
+        # pychecker
+        # coverage
+        # globalsub
+        package_data = {
+            '{{app_name}}': [
+            ],
+        },
+        install_requires=[
+        ],
+        scripts = [
+        ],
+        entry_points = dict(
+            console_scripts = [
+            ],
+        ),
+    )
+
